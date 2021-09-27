66edbfc9ae8e2f07e975c8b7f6180799f73a513f
DevExpressExampleBot <fakeEmail>
2021-09-27 15:19:14 +0300

    autogenerate VB

---
+++ VB/EnumItemsSourceBehaviorExample/App.xaml.vb
@@ -0,0 +1,13 @@
+Imports DevExpress.Xpf.Core
+Imports System.Windows
+
+Namespace EnumItemsSourceBehaviorExample
+
+    Public Partial Class App
+        Inherits Application
+
+        Private Sub OnAppStartup_UpdateThemeName(ByVal sender As Object, ByVal e As StartupEventArgs)
+        'DevExpress.Xpf.Core.ApplicationThemeHelper.ApplicationThemeName=Theme.Office201;
+        End Sub
+    End Class
+End Namespace
--- VB/EnumItemsSourceBehaviorExample/Common/FilterStringConverterExtension.vb
+++ VB/EnumItemsSourceBehaviorExample/Common/FilterStringConverterExtension.vb
@@ -1,14 +1,15 @@
-﻿Imports System
+Imports System
 Imports System.Linq
 Imports System.Windows.Markup
 Imports System.Collections.Generic
 
 Namespace EnumItemsSourceBehaviorExample.Common
-	Public Class FilterStringConverterExtension
-		Inherits MarkupExtension
 
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return New FilterStringConverter()
-		End Function
-	End Class
+    Public Class FilterStringConverterExtension
+        Inherits MarkupExtension
+
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return New FilterStringConverter()
+        End Function
+    End Class
 End Namespace
